7c06bac0f21f11c35c884a18981911dda602468c
github username <github email address>
2020-04-01 16:58:48 -0400

    commit

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,4 +2,15 @@ class Interest < ActiveRecord::Base
     has_many :member_interest_ads 
     has_many :members, through: :member_interest_ads 
     has_many :ads, through: :member_interest_ads 
+    # Methods to find the interest corresponding with the most ads. 
+    def ad_count 
+        self.ads.length 
+    end 
+    def self.most_ads 
+        self.maximum(ad_count) 
+    end 
+    def self.most_popular_interest_by_ad 
+        self.where("ad_id = ?", self.most_ads).first 
+    end 
+     
 end 
