f09655de4ddafead21857a5fcac464392e3b6277
github username <github email address>
2020-04-01 16:55:28 -0400

    commit

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,11 +3,11 @@ class Ad < ActiveRecord::Base
     has_many :members, through: :member_interest_ads 
     has_many :interests, through: :member_interest_ads 
     # Methods to find the ad corresponding with most interests. 
-    def interest_count 
-        self.interests.length 
+    def ads_count 
+        self.ads.length 
     end 
-    def self.most_interests 
-        self.maximum(interest_count) 
+    def self.most_ads 
+        self.maximum(ads_count) 
     end 
     def self.most_popular_ad_by_interest 
         self.where("interest_id = ?", self.most_ads).first 
