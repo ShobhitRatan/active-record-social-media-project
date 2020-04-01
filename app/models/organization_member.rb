83a4de30227de1b302262d21fb19294f768f22df
github username <github email address>
2020-04-01 14:13:52 -0400

    commit

--- app/models/organization_member.rb
+++ app/models/organization_member.rb
@@ -1,9 +1,17 @@
 class OrganizationMember < ActiveRecord::Base 
     belongs_to :organization 
     belongs_to :member 
-    # Method to get all organization's names.  
+    # Method to get all organization names.  
     def all_organization_names 
         self.organizations.map(&:name) 
     end 
-    # 
+    # Method to get all member names. 
+    def all_member_names 
+        self.members.map(&:name) 
+    end 
+    # Method to get the organization name of first member. 
+    def get_organization_of_first_member 
+        self.members.first.organization 
+    end 
+    
 end 
