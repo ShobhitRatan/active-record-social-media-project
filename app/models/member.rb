c7265345db3977008a05567996c6475069d2ad63
github username <github email address>
2020-04-01 10:48:37 -0400

    commit

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,9 @@ class Member < ActiveRecord::Base
     has_many :interests, through: :member_interest_ads 
     has_many :ads, through: :member_interest_ads 
     has_many :organizations, through: :organization_members 
+
+    # Method to get the members that are in highest number of organizations. 
+    def self.highest_number_of_organizations 
+        self.maximum(:organization_id) 
+    end 
 end 
