c2f43e11f03692b7bcdb36407ee0f78c649981a1
github username <github email address>
2020-04-01 14:21:09 -0400

    commit

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,13 @@
 class Organization < ActiveRecord::Base 
     has_many :organization_members 
     has_many :members, through: :organization_members 
+    # Method to return members associated with the organization. 
+    def member_count 
+        self.members.length 
+    end 
+    # Method to find members by their hometown. 
+    def members_by_hometown 
+        self.members.where("home_town = ?") 
+    end 
+    
 end 
