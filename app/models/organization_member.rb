568fea9f1dc0b5d0b513ec8478d9792406c44ff8
github username <github email address>
2020-04-01 14:02:02 -0400

    commit

--- app/models/organization_member.rb
+++ app/models/organization_member.rb
@@ -1,4 +1,9 @@
 class OrganizationMember < ActiveRecord::Base 
     belongs_to :organization 
     belongs_to :member 
+    # Method to get all organization's names.  
+    def all_organization_names 
+        self.organizations.map(&:name) 
+    end 
+    # 
 end 
